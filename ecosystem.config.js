199c8da6d9ae4d4c488fd719dd703c88946d2bc7
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-12-01 21:21:07 +0300

    setup sensor setup pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+
+    // Application description
+    {
+      name: 'ZSmartHome RPGPIO REST Service',
+      script: 'index.js',
+      watch: true,
+      env: {
+        NODE_ENV: 'development',
+        PORT: 8000,
+        PIN_NUMBER: 17
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: 'pi',
+      host: 'raspberrypi',
+      ref: 'origin/master',
+      repo: 'git@github.com:ZSmartHome/smart-rgpio.git',
+      path: '/home/pi/smart-rgpio',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js'
+    }
+  }
+};
--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require(`express`);
+const sensor = require("node-dht-sensor").promises;
+
+sensor.setMaxRetries(10);
+
+const DHT_TYPE = 11;
+
+const PORT = process.env.PORT || 3000;
+const IS_DEV = process.env.NODE_ENV !== 'production';
+const PIN_NUMBER = process.env.PIN_NUMBER || 17;
+
+if (IS_DEV) {
+  sensor.initialize({
+    test: {
+      fake: {
+        temperature: 21,
+        humidity: 60
+      }
+    }
+  });
+}
+
 const app = express();
-const port = 3000;
+app.get(`/`, (req, res) => sensor.read(DHT_TYPE, PIN_NUMBER).then(
+  (data) => res.send(data),
+  (err) => res.status(500).json(err)
+));
 
-app.get(`/`, (req, res) => res.send(`Hello World!`));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
